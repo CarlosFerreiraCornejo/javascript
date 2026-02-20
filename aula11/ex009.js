958a005234d41cef5e7e4f01eb88650b3d13f83a
CarlosFerreiraCornejo <carlosfcornejo@outlook.com>
2026-02-20 19:35:40 -0300

    Novos exercícios

---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'Chile'
+console.log(`Você nasceu no país ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Portanto você é brasileiro!')
+}
+else {
+    console.log('Portanto você é estrangeiro!')
+}
